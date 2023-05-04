30aa6a77e093e8a4aa1fd94df6e40bfc790d1217
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-04 10:23:24 +0100

    handling likes counts

--- mexme/tweets/views.py
+++ mexme/tweets/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -14,7 +16,8 @@ def tweet_list_view(request, *args, **kwargs):
     returning json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(
+        0, 1254)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
