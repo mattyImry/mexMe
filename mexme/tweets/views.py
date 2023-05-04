ab4669f5eb613eb834d71f3de32121812f69bd1a
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-04 15:00:24 +0100

    add logic for safe redirect

--- mexme/tweets/views.py
+++ mexme/tweets/views.py
@@ -1,10 +1,13 @@
 import random
-
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 
 def home_view(request, *args, **kwargs):
@@ -13,11 +16,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
-    next_url = request.POST.get("next") or None
+    next_url = request.POST.get(
+        "next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        if next_url is not None:
+        if next_url is not None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
