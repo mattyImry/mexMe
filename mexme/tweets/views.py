62bc0780f36a7f81357f4b58bce6fc2434fa9111
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-04 14:50:38 +0100

    set redirect form submission

--- mexme/tweets/views.py
+++ mexme/tweets/views.py
@@ -1,7 +1,7 @@
 import random
 
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Tweet
 from .forms import TweetForm
@@ -13,9 +13,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url is not None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
