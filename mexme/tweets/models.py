942cdd5aa490cbcaa988e37c79301c516bb6712b
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-10 11:00:37 +0100

    add user to model and migrate

--- mexme/tweets/models.py
+++ mexme/tweets/models.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.db import models
 import random
 
+User = settings.AUTH_USER_MODEL
+
 
 class Tweet(models.Model):
+    # many users can have many tweets
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
