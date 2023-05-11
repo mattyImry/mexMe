e0e02d72b3fb373493e3c76bc6f3f804e5a1afda
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-11 11:12:43 +0100

    add serializers.py

--- mexme/mexme/settings.py
+++ mexme/mexme/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 LOGIN_URL = "/login"
+MAX_TWEET_LENGTH = 240
 
 
 # Application definition
@@ -39,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # third-party
+
     'rest_framework',
-    # internal
+
     'tweets',
 ]
 
