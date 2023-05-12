ce48555349a0359b706d562e06d480c68bc1ede6
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-12 11:46:09 +0100

    add like in admin panel

--- mexme/tweets/admin.py
+++ mexme/tweets/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, tweetLike
 
 # Register your models here..
 
 
+class TweetLikeAdmin(admin.TabularInLine):
+    model = TweetLike
+
+
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['user__username', 'user__email']
 
