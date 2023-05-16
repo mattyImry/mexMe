dcb8783d797045d97802117df452e2ebc4339d29
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-16 12:27:36 +0100

    modify serializer to display retweet

--- mexme/tweets/serializers.py
+++ mexme/tweets/serializers.py
@@ -37,17 +37,11 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    content = serializers.SerializerMethodField(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
 
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes', 'is_retweet']
+        fields = ['id', 'content', 'likes', 'is_retweet', 'parent']
 
     def get_likes(self, obj):
         return obj.likes.count()
-
-    def get_content(self, obj):
-        content = obj.content
-        if obj.is_retweet:
-            content = obj.parent.content
-        return content
