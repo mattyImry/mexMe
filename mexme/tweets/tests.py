340812fd2758dbd43ddbe2ed3f8b271e8de5061c
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-16 14:21:15 +0100

    setting testing Tweet app

--- mexme/tweets/tests.py
+++ mexme/tweets/tests.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from .model import Tweet
 # Create your tests here.
+user = get_user_model()
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        
