aa664eaf1efe62a2879b3f08e22ed8921e186f87
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-17 18:43:32 +0100

    set testing for test_tweet_created

--- mexme/tweets/tests.py
+++ mexme/tweets/tests.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from .model import Tweet
+from .models import Tweet
 # Create your tests here.
-user = get_user_model()
+User = get_user_model()
+
 
 class TweetTestCase(TestCase):
     def setUp(self):
-        
+        self.user = User.objects.create_user(
+            username='hyj', password='something')
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content="my tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 1)
+        self.assertEqual(tweet_obj.user, self.user)
