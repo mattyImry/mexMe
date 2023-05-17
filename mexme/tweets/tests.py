386b20a527af41ab73f47b837497f043172a4066
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-17 18:59:48 +0100

    testing urls /api/tweets/

--- mexme/tweets/tests.py
+++ mexme/tweets/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from rest_framework.test import APIClient
+
 from .models import Tweet
 # Create your tests here.
 User = get_user_model()
@@ -10,8 +12,21 @@ class TweetTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username='hyj', password='something')
+        Tweet.objects.create(content="my first tweet", user=self.user)
 
     def test_tweet_created(self):
-        tweet_obj = Tweet.objects.create(content="my tweet", user=self.user)
-        self.assertEqual(tweet_obj.id, 1)
+        tweet_obj = Tweet.objects.create(
+            content="my sec tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 2)
         self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='something')
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
