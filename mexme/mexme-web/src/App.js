aaa3edf764a3c8be447cf684316ba51dbebdc656
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-23 13:29:26 +0100

    displaying tweets via react

--- mexme/mexme-web/src/App.js
+++ mexme/mexme-web/src/App.js
@@ -19,6 +19,15 @@ function loadTweets(callback) {
 xhr.send();
 }
 
+function Tweet(props){
+  const {tweet} = props
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+  return <div className={className}>
+          <p>{tweet.id} - {tweet.content}</p>
+
+  </div>
+}
+
 function App() {
   const [tweets, setTweets] = useState([]);
 
@@ -40,11 +49,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet, index)=>{
-            return <li>{tweet.content}</li>
+        <div>
+          {tweets.map((item, index)=>{
+            return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 bg-white text-dark'/>
           })}
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
