ab733bb227337b4ee7f8a71c041fa33af138bf9c
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-25 14:09:12 +0100

    add componenrts.js and index.js

--- mexme/mexme-web/src/App.js
+++ mexme/mexme-web/src/App.js
@@ -19,26 +19,6 @@ function loadTweets(callback) {
 xhr.send();
 }
 
-function Tweet(props){
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-          <p>
-            {tweet.id} - {tweet.content}
-          </p>
-
-          <div> 
-            <ActionBtn tweet={tweet} action={{type:'like'}} />
-          </div>
-  </div>
-}
-
-function ActionBtn(props){
-  const {tweet, action} = props
-  const className = props.className ? props.className : 'btn btn-primary btn-sm'
-  return action.type === 'like' ? <button className={className}>{tweet.likes} Likes </button> : null
-
-};
 
 function App() {
   const [tweets, setTweets] = useState([]);
