0c3daca6c09dd62a16cfb96d22011568a8216be7
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-26 11:34:54 +0100

    add ref for textarea components.js

--- mexme/mexme-web/src/tweets/components.js
+++ mexme/mexme-web/src/tweets/components.js
@@ -3,21 +3,30 @@ import React, { useEffect, useState } from "react";
 import { loadTweets } from "../lookup/components";
 
 export function TweetComponent(props) {
+    const textAreaRef = React.createRef();
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(event);
+        const newValue = textAreaRef.current.value;
+        textAreaRef.current.value = "";
     };
 
     return (
         <div className={props.className}>
             <div className="col-12 mb-3">
                 <form onSubmit={handleSubmit}>
-                    <textarea className="form-control" name="tweet"></textarea>
+                    <textarea
+                        ref={textAreaRef}
+                        required={true}
+                        className="form-control"
+                        name="tweet"
+                    ></textarea>
                     <button type="submit" className="btn btn-primary my-3">
                         Tweet
                     </button>
                 </form>
             </div>
+            <TweetsList />
         </div>
     );
 }
--- mexme/mexme-web/src/tweets/index.js
+++ mexme/mexme-web/src/tweets/index.js
@@ -1,7 +1,8 @@
-import { ActionBtn, Tweet, TweetsList } from "./components";
+import { ActionBtn, Tweet, TweetsList, TweetComponent } from "./components";
 
 export{
     ActionBtn,
     Tweet,
-    TweetsList
+    TweetsList,
+    TweetComponent
 }
