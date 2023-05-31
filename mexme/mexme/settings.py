da214f1dd67c8dc03f19a6cb0d195117df6c481b
Matteo Imeri <matteo.imeri@gmail.com>
2023-05-31 15:14:25 +0100

    settingstatic file django

--- mexme/mexme/settings.py
+++ mexme/mexme/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join()
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
