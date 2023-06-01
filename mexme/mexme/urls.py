ca5b27fb707a6dbba5bf1fcb940e02fcf4ff805f
Matteo Imeri <matteo.imeri@gmail.com>
2023-06-01 08:22:15 +0100

    set urls for static

--- mexme/mexme/urls.py
+++ mexme/mexme/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 
@@ -34,3 +36,6 @@ urlpatterns = [
     # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
     path('api/tweets/', include('tweets.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
